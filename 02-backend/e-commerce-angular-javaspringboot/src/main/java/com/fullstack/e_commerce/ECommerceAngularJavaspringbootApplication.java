3308b02d9996689365b4e594fb6dc2e355d6ade6
codershona <falgunimst95@gmail.com>
2024-08-02 07:44:34 +0100

    ecommerce backend springboot project setup, database tables setup using, developed jpa entitites

---
+++ 02-backend/e-commerce-angular-javaspringboot/src/main/java/com/fullstack/e_commerce/ECommerceAngularJavaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAngularJavaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceAngularJavaspringbootApplication.class, args);
+	}
+
+}
---
+++ 02-backend/e-commerce-angular-javaspringboot/src/test/java/com/fullstack/e_commerce/ECommerceAngularJavaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAngularJavaspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
